af22ff98b7bcc39f75e8c66fe64592e7b0e09cae
TheMcnafaha <jafetguzmanflores@gmail.com>
2023-11-30 20:11:15 -0500

    further tests

--- src/routes/404.tsx
+++ src/routes/404.tsx
@@ -4,8 +4,8 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <>
-      <h1>You messed up G</h1>
-      <h2>Like real bad</h2>
+      <h1>Im the 404 of /</h1>
+      <p>This even has HMR!!!</p>
     </>
   );
 });
