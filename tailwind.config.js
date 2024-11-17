fe08cb27870799b32640afecc938c87b06ff06cf
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-11-16 23:25:13 -0500

    feat(tailwind.config): adds custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-gray": "hsl(0, 0%, 98%)",
+        "dark-gray": "hsl(0, 0%, 52%)",
+        "dark-blue": " hsl(209, 23%, 22%)",
+        "very-dark-blue": " hsl(207, 26%, 17%)",
+        "blue-darko": " hsl(200, 15%, 8%)",
+      },
+    },
   },
   plugins: [],
 };
