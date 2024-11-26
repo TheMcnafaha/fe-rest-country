d704ae8c3ece1a4e8a8530d38c6333607cc66eff
Jafet Natanael Guzman Flores <jafetguzmanflores@gmail.com>
2024-11-26 00:01:22 -0500

    fix(context-wrapper): query works again
    apperently the original call took too long, so the server killed it

--- src/components/countries/context-wrapper.tsx
+++ src/components/countries/context-wrapper.tsx
@@ -14,12 +14,27 @@ export const AllCountriesContext =
 export const ContextWrapper = component$(() => {
   const allCountriesSig = useSignal<CountryResponse[]>([]);
   useTask$(async () => {
-    const queryString = "https://restcountries.com/v3.1/all";
-    const res = await fetch(queryString);
-    const data = await res.json();
-    const date = new Date();
-    console.log("QUERYING: ", date.toLocaleTimeString());
-    allCountriesSig.value = data;
+    let json;
+
+    try {
+      const response = await fetch(
+        "https://restcountries.com/v3.1/all?fields=name,flags,population,region,subregion,capital,tld,languages,borders,cca3,currencies",
+      );
+      json = await response.json();
+    } catch (error) {
+      if (error instanceof SyntaxError) {
+        // Unexpected token < in JSON
+        console.log("There was a SyntaxError", error);
+      } else {
+        console.log("There was an error", error);
+      }
+    }
+
+    if (json) {
+      const date = new Date();
+      console.log("QUERYING: ", date.toLocaleTimeString());
+      allCountriesSig.value = json;
+    }
   });
   useContextProvider(AllCountriesContext, allCountriesSig);
   return (
